40b3e18feda99b3e2826604fcc60aa53c14bf2ac
Joshua <joshaw66@gmail.com>
2020-01-21 11:32:52 -0700

    completed getFullNames

--- index.js
+++ index.js
@@ -178,8 +178,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let names = [];
+  runners.forEach((elem) => {
+    names.push(`${elem.last_name}, ${elem.first_name}`);
+  });
+  return names;
 }
 
 /**
